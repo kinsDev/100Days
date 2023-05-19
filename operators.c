5d99259042c054aadcfed1ce5abae85ce07c1725
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-05-19 09:19:38 +0300

    operation C-eeing this though

---
+++ operators.c
@@ -0,0 +1,11 @@
+/**
+ * a++ - increments a variable after using it
+ * ++a - increments a variable before using it
+*/
+#include <stdio.h>
+int main (void) {
+    int a = 2;
+    int b;
+    b = a++;
+    return 0;/*marking the end of code - good practice*/
+}
