6dda357245af7014d723b6a07d5484d5ab7dd40e
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-05-19 15:10:30 +0300

    operation C-eeing this through

--- operators.c
+++ operators.c
@@ -2,10 +2,21 @@
  * a++ - increments a variable after using it
  * ++a - increments a variable before using it
 */
-#include <stdio.h>
+/*#include <stdio.h>
 int main (void) {
     int a = 2;
     int b;
     b = a++;
-    return 0;/*marking the end of code - good practice*/
-}
+    return 0;/*marking the end of code - good practice
+}*/
+/*a ? b : c if a is evaluated to true then b statement is executed otherwise C is executed*/
+
+    /**% = ld = Long int. ! = not? \n = new line*/
+
+    /**Lets check the sizeof an integer*/
+    #include <stdio.h>
+    int main(void) {
+        int age = 37;
+        printf("%ld\n", sizeof(age));
+        printf("%ld", sizeof(int));
+    }
