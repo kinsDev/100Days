d3b7ff13cfded5844fe9b2f735484f138a09d906
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-05-22 17:40:58 +0300

    C-eeing this through

--- operators.c
+++ operators.c
@@ -9,14 +9,36 @@ int main (void) {
     b = a++;
     return 0;/*marking the end of code - good practice
 }*/
+
+
 /*a ? b : c if a is evaluated to true then b statement is executed otherwise C is executed*/
 
     /**% = ld = Long int. ! = not? \n = new line*/
 
+
     /**Lets check the sizeof an integer*/
-    #include <stdio.h>
+    /*#include <stdio.h>
     int main(void) {
         int age = 37;
         printf("%ld\n", sizeof(age));
         printf("%ld", sizeof(int));
-    }
+    }*/
+
+
+/**The ternary operator
+ * <condition> ? <expression> : <expression>
+ * e.g
+ * a ? b : c
+ * basically, if a is evaluated to true, then b is executed, otherwise c is executed 
+*/
+
+
+/**The Size of operator
+ * It returns the size of operand you pass
+*/
+#include <stdio.h>
+int main(void){
+    int age  = 37;
+    printf("%ld\n", sizeof(int));
+    printf("%ld", sizeof(age));
+}
