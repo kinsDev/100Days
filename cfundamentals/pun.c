8b6638049dbc58ebc50e2e1a94f278251d13535d
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-05-29 15:07:08 +0300

    simple C program

---
+++ cfundamentals/pun.c
@@ -0,0 +1,12 @@
+/**
+ * Instead of the usual hello world
+ * We will be wiriting the traditional C pun
+*/
+
+#include <stdio.h> //stdio - standard input output library is a library to display standard and output files.
+int main(void){ //int is used to show that the result of the main function will be an integer
+    printf("To C, or not to C: that is the question.\n");
+    printf("Brevity is the soul of wit.\n --Shakespeare.\n");// \n - new line after end of a sentence
+
+    return 0;//end of program running
+}
