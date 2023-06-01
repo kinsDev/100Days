3e4ed5d881aaab2826c4ac1a3e84ff10eefa2a5e
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-01 23:37:41 +0300

    programming projects 1

--- Programming_Projects/pp7.c
+++ Programming_Projects/pp7.c
@@ -12,3 +12,38 @@ is decreased by the amount of the payment, but increased by the balance times th
 interest rate. To find the monthly interest rate, convert the interest rate entered by the user to
 a percentage and divide it by 12
 */
+
+#include <stdio.h>
+
+int main(void){
+    float interestRate, monthlyPayment, loan;
+    float balance;
+
+    printf("Enter amount of loan: ");
+    scanf("%f", &loan);
+    printf("Enter interest rate: ");
+    scanf("%f", &interestRate);
+    printf("Enter monthly payment: ");
+    scanf("%f", &monthlyPayment);
+
+    interestRate /= 100;
+
+    balance = loan;
+
+    balance -= monthlyPayment;
+    balance += balance * (interestRate / 12);
+
+    printf("Balance remaining after first payment: $%.2f\n", balance);
+
+    balance -= monthlyPayment;
+    balance += balance * (interestRate / 12);
+
+    printf("Balance remaining after second payment: $%.2f\n", balance);
+
+    balance -= monthlyPayment;
+    balance += balance * (interestRate / 12);
+
+    printf("Balance remaining after third payment: $%.2f\n", balance);
+
+    return 0;
+}
