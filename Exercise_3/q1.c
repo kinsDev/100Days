60dc58eb003fca041ddaed34d45738ad6c1e6092
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-09 12:26:44 +0300

    Exercise 3 questions and answers

---
+++ Exercise_3/q1.c
@@ -0,0 +1,13 @@
+/**
+ * If i and j are positive integers, does (-i) /j always have the same value as - (i/j ) ? Jus¬
+tify your answer.
+*/
+
+
+#include <stdio.h>
+int main(void)
+{
+    int i = 2, j = 4;
+    printf("%d\n", (-(i))/j);
+    return 0;
+}
--- scrabble.c
+++ scrabble.c
@@ -1,5 +1,5 @@
 /**
- * In the SCRABBLE Crossword Came, players form words using small tiles, each containing
+ * In the SCRABBLE Crossword Game, players form words using small tiles, each containing
 a letter and a face value. The face value varies from one letter to another, based on the letter’s rarity. (Here are the face values: I: AEILNORSTU. 2: DG, 3: BCMP, 4: FHVWY, 5: K. 8: JX, 10: QZ.) Write a program that computes the value of a word by summing the values
 of its letters:
 Enter a word: pitfall
