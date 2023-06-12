7055ee9fc5ee565e74dd1afad410f32eb028ba6e
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-12 13:24:01 +0300

    Its just keeps getting better and better

--- Programming_Projects_3/pp3_upc_reversal.c
+++ Programming_Projects_3/pp3_upc_reversal.c
@@ -3,3 +3,15 @@
 number without using arithmetic to split the number into digits. Him: See the upc. c program of Section 4.1
 */
 
+#include<stdio.h>
+int main(void)
+{
+    int i, j, k;
+
+    printf("Enter a three digit number: ");
+    scanf("%1d%1d%1d", &i, &j, &k);
+
+    printf("The reversed number is: %1d%1d%1d", k, j, i);
+
+    return 0;
+}
