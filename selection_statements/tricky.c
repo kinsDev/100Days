fffbd564379f78a9ff9979bf74fa6370b76870d1
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-13 13:16:07 +0300

    Clever programmers, trick codes

---
+++ selection_statements/tricky.c
@@ -0,0 +1,7 @@
+/**
+ * Clever programmers sometimes exploit the fact that the relational and equality
+operators return integer values. For example, the value of the expression (i >= j )
++ (i == j ) is either 0, 1, or 2, depending on whether i is less than, greater than,
+or equal to j. respectively. Tricky coding like this generally isn’t a good idea, how¬
+ever; it makes programs hard to understand.
+*/
