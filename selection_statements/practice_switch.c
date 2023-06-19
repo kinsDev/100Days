1595c788fb94da4517bb4bb9ef98fbd2466f4f2d
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-19 13:23:06 +0300

    update on the switch statements

--- selection_statements/practice_switch.c
+++ selection_statements/practice_switch.c
@@ -1,7 +1,36 @@
 /**
  * switch (expression){
- * case constant-expression: statement
+ * case: constant-expression: statement;
  * ...
- * case constant-expression: statement
+ * case: constant-expression: statement;
+ * ...
+ * default: statements;
  * }
 */
+#include <stdio.h>
+int main(void)
+{
+    int grade;
+
+    printf("Enter your score out of 5: ");
+    scanf("%d", &grade);
+
+    switch(grade){
+        case 5: printf("Perfect");
+                break;
+        case 4: printf("Excellent");
+                break;
+        case 3: printf("Good");
+                break;
+        case 2: printf("Average");
+                break;
+        case 1: printf("Poor");
+                break;
+        case 0: printf("Fail");
+                break;
+        default: printf("Invalid score!");
+                 break;
+    }
+
+    return 0;
+}
