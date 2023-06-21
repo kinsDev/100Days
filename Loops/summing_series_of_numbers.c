41dd61b45df5fc6daee383376f65664c8082dff4
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-21 16:51:46 +0300

    summing series of numbers

---
+++ Loops/summing_series_of_numbers.c
@@ -0,0 +1,29 @@
+/**
+ * As a second example of the while statement, let’s write a program that sums a
+series of integers entered by the user. Here’s what the user will see:
+
+This program sums a series of integers.
+Enter integers (0 to terminate): 8 23 71 5 0
+The sum is: 107
+Clearly we’ll need a loop that uses scanf to read a number and then adds the
+number to a running total.
+Letting n represent the number just read and sum the total of all numbers pre¬
+viously read, we end up with the following program:
+*/
+
+#include <stdio.h>
+int main(void)
+{
+    int n, sum = 0;
+
+    printf("Enter integers (0 to terminate): ");
+    scanf("%d", &n);
+
+    while (n != 1){
+        sum += n;
+        scanf("%d", &n);
+    }
+    printf("The sum is: %d", sum);
+
+    return 0;
+}
