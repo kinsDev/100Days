efac33bd9f3c3544d558123db371e7dcb7f8ebeb
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-25 22:08:41 +0300

    update

--- random_quizz_practice/netwon.c
+++ random_quizz_practice/netwon.c
@@ -1,23 +1,18 @@
 /**
- * Write a program that uses Newton's method to compute the square root of a positive float¬
-ing-point number:
+ * Write a program that uses Newton's method to compute the square root of a positive float¬ing-point number:
 Enter a positive number: 3
 Square root: 1.73205
 Let x be the number entered by the user. Newton’s method requires an initial guess y for the
 square root of.v (we’ll use y = 1). Successive guesses are found by computing the average of
 y and x/y. The following table shows how the square root of 3 would be found:
-Average of
-x y x/y y and x/y
-3 1 3
-3 2 1.5
-3 1.75 1.71429
-3 1.73214 1.73196
-3 1.73205 1.73205
-2
-1.75
-1.73214
-1.73205
-1.73205
+
+x    y         x/y y         Average of y and x/y
+3    1         3             2
+3    2         1.5           1.75
+3    1.75      1.71429       1.73214
+3    1.73214   1.73196       1.73205
+3    1.73205   1.73205       1.73205
+
 Note that the values of y get progressively closer to the true square root of x. For greater
 accuracy, your program should use variables of type double rather than float. Have the
 program terminate when the absolute value of the difference between the old value ofy and
