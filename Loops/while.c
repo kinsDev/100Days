6983ef723b5dbf8cd8430dcd7e5429ebd67c3d09
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-26 10:38:07 +0300

    update

--- Loops/while.c
+++ Loops/while.c
@@ -2,3 +2,14 @@
  * while (the expression is true) {execute this statement};
  * The statement is not executed when the expression is false.
 */
+
+#include <stdio.h>
+int main(void)
+{
+    int n, i;
+    n = 10;
+    i = 1;
+    while (i < n){
+        printf("%d\n", i = i * 2);
+    }   
+}
