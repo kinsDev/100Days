bf1285329958d79d036a2c1ae6844a8981e6e54b
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-27 09:48:40 +0300

    printing square using odd numbers

--- Loops/printing_square_of_odds.c
+++ Loops/printing_square_of_odds.c
@@ -1,9 +1,40 @@
 /**
- * We can use this program to illustrate an important point about the for state¬
-ment: C places no restrictions on the three expressions that control its behavior.
+We can use this program to illustrate an important point about the for statement: 
+C places no restrictions on the three expressions that control its behavior.
 Although these expressions usually initialize, test, and update the same variable,
-there’s no requirement that they be related in any way. Consider the following ver¬
-sion of the same program:
+there’s no requirement that they be related in any way. Consider the following 
+version of the same program:
 */
+/*#include <stdio.h>
+int main(void)
+{
+    int i, n;
+    printf("This program prints a table of squares.\n");
+    printf("Enter a number of entries in the table: ");
+    scanf("%d", &n);
 
+    for(i = 1; i <=n; i++){
+        printf("%d\t%d\n", i, i*i);
+    }
+    return 0;
+}*/
 
+#include <stdio.h>
+int main(void)
+{
+    int i, n, odd, square;
+    printf("This program prints a table of squares.\n");
+    printf("Enter a number of entries in the table: ");
+    scanf("%d", &n);
+
+    i = 1;
+    odd = 3;
+
+    for(square = 1; i <= n; odd += 2){
+        printf("%d\t%d\n", i, square);
+        i++;
+        square += odd;
+    }
+
+    return 0;
+}
