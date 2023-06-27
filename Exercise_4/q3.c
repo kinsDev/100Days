278373446f8320e471245cfc885b0736fa90d563
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-27 15:14:10 +0300

    calculating sum of even numbers from 0 - 9

---
+++ Exercise_4/q3.c
@@ -0,0 +1,28 @@
+/**
+What output does the following program fragment produce?
+
+sum = 0;
+for (i = 0; i < 10; i++) {
+if (i % 2)
+continue;
+sum += i;
+}
+printf("%d\n", sum);
+
+*/
+
+#include <stdio.h>
+int main(void)
+{
+    int sum, i, n;
+
+    sum = 0;
+    for(i = 0; i < 10; i++){
+        if(i % 2)
+        continue;
+        sum += i;
+    }
+    printf("%d\n", sum);
+
+    return 0;
+}
