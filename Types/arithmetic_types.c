839fae18ead5bcb3660411e8156662625e0a8fa4
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-29 22:55:31 +0300

    arithmetic types in C

---
+++ Types/arithmetic_types.c
@@ -0,0 +1,27 @@
+/**
+The integer types and floating types are collectively known as arithmetic types.
+
+arithmetic types in C89, divided into categories and sub¬categories:
+* Integral types
+• char
+• Signed integer types (signed char, short int, int, long int)
+• Unsigned integer types (unsigned char, unsigned short int,
+unsigned int. unsigned long int)
+• Enumerated types
+* Floating types (float, double, long double)
+
+
+C99 has a more complicated hierarchy for its arithmetic types:
+* Integer types
+• char
+• Signed integer types, both standard (signed char, short int, int,
+long int. long long int) and extended
+• Unsigned integer types, both standard (unsigned char, unsigned
+short int, unsigned int, unsigned long int, unsigned
+long long int, _Bool) and extended
+• Enumerated types
+* Floating types
+• Real floating types (float, double, long double)
+• Complex types (float _Complex, double ^Complex, long dou¬
+ble Complex)
+*/
