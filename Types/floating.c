9f82ca8f04b013e1d3fabfbd54b923fd46efa4cd
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-29 21:36:46 +0300

    floats

---
+++ Types/floating.c
@@ -0,0 +1,14 @@
+/**
+The integer types aren’t suitable for all applications. Sometimes we’ll need vari¬
+ables that can store numbers with digits after the decimal point, or numbers that are
+exceedingly large or small. Numbers like these tire stored in floating-point format
+
+float           Single-precision floating-point
+double          Double-precision floating-point
+long double     Extended-precision floating-point
+
+float is suitable when the amount of precision isn't critical (e.g calculating tempera¬
+tures to one decimal point), double provides greater precision—
+enough for most programs, long double, which supplies the ultimate in preci¬
+sion. is rarely used.
+*/
