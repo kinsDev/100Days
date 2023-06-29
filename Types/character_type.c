9216e00d99e657d47cc1260bb98f5641899c7759
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-29 22:49:24 +0300

    character types

---
+++ Types/character_type.c
@@ -0,0 +1,17 @@
+/**
+char ...;
+
+A variable of type char can be assigned any single character:
+char Ch;
+ch = 'a';     lower-case a
+ch = 'A';     upper-case A
+ch = '0’;     zero
+ch = ' ';     space
+
+NB: Notice that character constants are enclosed in single quotes, not double quote
+    C treats characters as small integers.
+
+C allows the use of the words signed and unsigned to modify char:
+signed char sch;
+unsigned char uch;
+*/
