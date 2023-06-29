bf4279cbbff0d01e9c19dd2f6efa1f60a42fe9ce
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-29 17:42:47 +0300

    interger types notes

---
+++ Types/integer_types.c
@@ -0,0 +1,17 @@
+/**
+Values of an integer type are whole numbers.
+The integer types, in turn, are divided into two categories: signed and unsigned.
+
+the following six combinations actually produce different types:
+short int
+unsigned short int - NB: order doesn't matter - e.g: short unsigned int
+int
+unsigned int
+long int
+unsigned long int
+
+long signed int - long int
+
+NB: integers are always signed unless otherwise specified.
+    int doesn't have to be included,it can be omitted. 
+*/
