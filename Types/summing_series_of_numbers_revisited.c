5240e1c1f8bf8c13def5d8075bf2a656626e084a
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-29 18:21:49 +0300

    summing series of numbers using the long integer

---
+++ Types/summing_series_of_numbers_revisited.c
@@ -0,0 +1,32 @@
+/**
+In Section 6.1, we wrote a program that sums a series of integers entered by the
+user. One problem with this program is that the sum (or one of the input numbers)
+might exceed the largest value allowed for an int variable. Here’s what might
+happen if the program is run on a machine whose integers are 16 bits long:
+
+This program sums a series of integers.
+Enter integers (0 to terminate): 10000 20000 30000 0
+The sum is: -5536
+
+The sum was 60,000, which wouldn’t fit in an int variable, so overflow occurred.
+When overflow occurs with signed numbers, the outcome is undefined. In this
+case, we got an apparently meaningless number. To improve the program, let’s
+switch to long variables.
+*/
+#include <stdio.h>
+int main(void)
+{
+    long n, sum = 0;
+
+    printf("This program sums a series of integers.\n");
+    printf("Enter integers (0 to terminate): ");
+    scanf("%ld", &n);
+
+    while(n != 0){
+        sum += n;
+        scanf("%ld", &n);
+        
+    }
+    printf("The sum is: %ld\n", sum);
+    return 0;
+}
