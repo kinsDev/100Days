84a80116f69bf5088c7afaa79091af531ad31a2a
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-30 17:33:14 +0300

    last update for the word count

--- Programming_Projects_7/word_count.c
+++ Programming_Projects_7/word_count.c
@@ -32,3 +32,25 @@ int main(void)
 
     return 0;
 }
+
+/**
+Two variables are declared:
+
+total_length: to store the sum of the lengths of all the words.
+word_count: to keep track of the number of words in the sentence.
+The program prompts the user to enter a sentence.
+
+A while loop is used to read each character entered by the user until a newline character ('\n') is encountered.
+
+Within the loop, an if statement is used to check if the character is not a space. If it's not a space, it means it's part of a word, so the total_length is incremented.
+
+If the character is a space, it means the current word has ended, so the word_count is incremented.
+
+After the loop, the word_count is incremented by 1 to account for the last word, which may not be followed by a space.
+
+The average word length is calculated by dividing the total_length by the word_count and stored in the average variable.
+
+Finally, the program prints the average word length to one decimal place using the printf() function.
+
+This modified code considers punctuation marks to be part of the words they are attached to, as stated in the problem statement.
+*/
