6b3bd0bb283af21783220fe9fb3d51f8fc0b5e05
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-30 23:56:37 +0300

    one dimensional arrays

---
+++ Arrays/one_dimensional_arrays.c
@@ -0,0 +1,15 @@
+/**
+An array is a data structure containing a number of data values, all of which have
+the same type. These values, known as elements, can be individually selected by
+their position within the array.
+
+To declare an array, we must specify the type of the array’s elements and the
+number of elements. For example, to declare that the array a has 10 elements of
+type int. we would write
+int a[10];
+
+using a macro to define the length of an array is an excellent practice:
+#define N 10
+•••
+int a [N] ;
+*/
