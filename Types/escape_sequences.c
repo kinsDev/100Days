6a2fd4647512157505cb953bd48399ad5f4cee54
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-06-30 11:00:16 +0300

    escape sequences

---
+++ Types/escape_sequences.c
@@ -0,0 +1,16 @@
+/**
+There are two kinds of escape sequences: character escapes and numeric escapes.
+
+Name Escape Sequence
+Alert (bell)         \a
+Backspace            \b
+Form feed            \f
+New line             \n
+Carriage return      \r
+Horizontal lab       \t
+Vertical tab         \v
+Backslash            \\
+Question mark        \?
+Single quote         \'
+Double quote         \"
+*/
