4aee9b645a7e40343a800f8b53dde884cd1b9d2a
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-01 09:37:37 +0300

    update on reversing a series of numbers

--- Arrays/reversing_series_numbers.c
+++ Arrays/reversing_series_numbers.c
@@ -7,26 +7,25 @@ the numbers in reverse order:
 Enter 10 numbers: 34 82 49 102 7 94 23 11 50 31
 In reverse order: 31 50 11 23 94 7 102 49 82 34
 */
-
 #include <stdio.h>
 
 #define N 10
 
 int main(void)
 {
-    int a[N];//number of elements in array
-    int i;//array position
+    int a[N];//array with ten elements
+    int i;//loop through the elements
 
-    printf("Enter %d numbers: ", N);//storing numbers in the array as they read
+    printf("Enter %d numbers: ", N);
     for(i = 0; i < N; i++){
-        scanf("%d", &a[i]);//reads into array a as it increments
+        scanf("%d", &a[i]); //we are reading the looped data into the array a
     }
 
-    printf("In reverse order: ");
-    for(i = N - 1; i >= 0; i--)
-    { // basically picking the listed numbers from the last one to the first one
-        printf("%d", a[i]);
+    printf("In recerse order: ");
+    for(i = N - 1; i >= 0; i--){
+        printf("%d", a[i]); // writes/ outputs the read data
         printf("\n");
     }
+
     return 0;
 }
