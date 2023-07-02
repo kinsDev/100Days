7e9c7ea6aa08f7b7d2a54f4bc664ac130e7faf14
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-02 17:43:10 +0300

    function definitions

---
+++ Functions/defining_calling_functions/function_definition.c
@@ -0,0 +1,23 @@
+/**
+ * return-type function-name(arguments)
+ * {
+ *    declarations
+ *    statements
+ * }
+ *
+ * Return type of a function is the type of value the function returns.
+ * functions cannot return arrays
+ * specifying that the return-type is void indicates that the function cannot return a value
+ * do not omitt the return type
+ */
+
+
+/*
+double average(double a, double b)
+{
+    double sum;    declaration - we are declaring sum to be of type double
+
+    sum = a + b;     statement;
+    return sum / 2;  statement;
+}
+*/
