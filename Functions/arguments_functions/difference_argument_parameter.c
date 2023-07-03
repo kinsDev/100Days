c73edfa5e7a84c2aecaa34c447d802a215b1cd8e
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-03 13:14:29 +0300

    The difference between arguments and parameters

---
+++ Functions/arguments_functions/difference_argument_parameter.c
@@ -0,0 +1,11 @@
+/**
+ * Parameters
+appear in function definitions; they’re dummy names that represent values to be
+supplied when the function is called.
+
+Arguments are expressions that appear in
+function calls.
+
+In C. arguments are passed by value: when a function is called, each argument
+is evaluated and its value assigned to the corresponding parameter.
+*/
