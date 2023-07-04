cf1da1ec472969369472cb183b815e12e550c4ec
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-04 11:17:35 +0300

    still learning recursion

--- Functions/recursion/recursion.c
+++ Functions/recursion/recursion.c
@@ -11,23 +11,41 @@
  * While using recursion, make sure that it has a base (exit) condition; otherwise, the program will go into the infinite loop.
  *
  * The recursion contains two cases in its program body.
- * - Base case: is a specific condition in the function. When it is met, it terminates the recursion.
- * - Otherwise it goes into an infinite loop.
+ * - Base case: is an exit condition. Otherwise it goes into an infinite loop.
  * - Recursive case: The part of code inside the recursive function executed repeatedly while calling the recursive function.
  */
 
 
 /**
  * BASIC SYNTAX OF RECURSION
-void recusrive_fun(void)
+void recursive_fun(void) recursive function
 {
     Base_case;           stopping condition
 
-    recursive_fun();       recursive call/ repetition
+    recursive_fun();      recursive call
 }
 
 int main(void)
 {
     recursive_fun();       function call
+}
+*/
+
+/**
+ * In the code above, there is a recursive function and inside the recursive function
+ * there is a recursive call which calls the recursive function until a certain condition is met.
+ * If the condition get satisfied, the program control goes for the remaining statements and stops the program. 
+*/
+
+/*
+void recursive_function(void)
+{
+    //base case - execution
+    if base_case = true;
+    return;
+
+    else
+    //recursive case - keeps repeating until the recursive call turns out to be true
+    return code_for_recursion;
 }
 */
