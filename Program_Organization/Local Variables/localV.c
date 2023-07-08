afa6354439cfd17c72df25679db9d0222c52f3c7
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-08 14:09:48 +0300

    static local variables

---
+++ Program_Organization/Local Variables/localV.c
@@ -0,0 +1,19 @@
+/**
+ * A variable declared inside the body of a function is called a local variable
+*/
+
+//An exmaple:
+/*
+int sum_digits(int n)
+{
+    int sum = o;
+
+    while(n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
+*/
