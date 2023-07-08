3cc3c65a7ff765482498b686d712424429028b86
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-08 14:15:09 +0300

    static local variables

---
+++ Program_Organization/Local Variables/staticV.c
@@ -0,0 +1,15 @@
+//Static variables store information of the variable permanently
+/*
+A variable with a static storage duration retains its value throughout the
+execution of the program.
+*/
+
+//An example
+void f(void)
+{
+    static int i; /* static local variable */
+    ...
+}
+
+//In a nutshell a static variable is a place to hide information from a function
+//but retains that information for a future function call
