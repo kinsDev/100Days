0df63d87c6125dfab51b80c5c19c1c230f047e7a
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-10 11:51:37 +0300

    organizing a C program

---
+++ Program_Organization/External Variables/organizing_program.c
@@ -0,0 +1,12 @@
+/**
+There are several ways to organize a C program:
+
+#include directives
+#define directives
+Type definitions
+Declarations of external variables
+Prototypes for functions other than main
+Definition of main
+Definitions of other functions
+
+*/
