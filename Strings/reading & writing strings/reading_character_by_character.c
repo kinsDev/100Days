e5af5ad6826a2c8904cbcf3aea9d2f00476601df
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-15 11:45:28 +0300

    update

--- Strings/reading & writing strings/reading_character_by_character.c
+++ Strings/reading & writing strings/reading_character_by_character.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-int line_reader(char str[], int n)
+// n is the number of characters in array str
+// i is used to loop through the conditions
+// ch is used to represent character which is read as an integer value
+// ch is used to represent getchar() function
+/**
+ * while ch is not equal to new line, do this:
+ * while i is less than elements in str array increment those elements
+ * and assign them to character ch
+*/
+int read_line(char str[], int n)
 {
     int i = 0, ch;
-    while((ch = getchar()) != '\n') //stops reading at first new line character
+    while((ch = getchar()) != '\n')
     {
         if(i < n)
         {
             str[i++] = ch;
         }
-        str[i] = '\0';             //terminates string
-        return i;                  //number of characters stored
     }
+    str[i] = '\0';
+    return i;
 }
 
 int main(void)
