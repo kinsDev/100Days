7e974d730a8702181303a7be270304a9193efdf9
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-07-15 11:54:38 +0300

    final update

--- Strings/reading & writing strings/reading_character_by_character.c
+++ Strings/reading & writing strings/reading_character_by_character.c
@@ -5,10 +5,11 @@
 // ch is used to represent character which is read as an integer value
 // ch is used to represent getchar() function
 /**
- * while ch is not equal to new line, do this:
- * while i is less than elements in str array increment those elements
- * and assign them to character ch
-*/
+ * (ch = getchar()) != '\n' reads a character and checks if it is not equal to a newline character.
+ * If i is less than n, the character ch is stored in the str array at the current index i, and i is incremented.
+ * After the loop finishes, the null character ('\0') is appended to the end of the str array to mark the end of the string.
+ * Finally, the function returns the value of i, which represents the number of characters read.
+ */
 int read_line(char str[], int n)
 {
     int i = 0, ch;
