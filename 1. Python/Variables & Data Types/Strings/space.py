7922248d5190aa5124ade589eb7de97a669922a8
kinsDev <kinsleygitonga@gmail.com>
2023-08-10 20:24:52 +0300

    Strpping white spaces

--- 1. Python/Variables & Data Types/Strings/space.py
+++ 1. Python/Variables & Data Types/Strings/space.py
@@ -1,3 +1,5 @@
+#tabs and newline
+
 print("Python")
 print("\n")
 
@@ -9,3 +11,17 @@ print("\n")
 
 print("Languages:\n\tPython\n\tC\n\tJavascript")
 print("\n")
+
+
+#dealing with spaces
+favorite_language = " Python"
+print(favorite_language) #the extra space remains
+
+print(favorite_language.lstrip()) #the extra space on the left is removed
+
+print(favorite_language) #the extra space comes back as it was temporarily removed
+
+favorite_language = favorite_language.lstrip() #we asign the favorite space value to be the one whose space has been removed on the left
+print(favorite_language) #now there is no extra space
+print(favorite_language)
+print(favorite_language)
