e4ec6bc5520074a3749bcfe80511eb93a4f9f861
kinsDev <kinsleygitonga@gmail.com>
2023-08-10 19:58:21 +0300

    Variables and data types

---
+++ 1. Python/Variables & Data Types/simple_message.py
@@ -0,0 +1,5 @@
+simple_message = "Hello there, my name is Kinsley Kaimenyi, and welciome to my programming journey in Python";
+print(simple_message);
+
+simple_message = "changing the value of our variable";
+print(simple_message);
