9cadbcf3cbe346424741a786df0235328097806b
kinsDev <kinsleygitonga@gmail.com>
2023-08-10 20:12:26 +0300

    Adding Whitespace to Strings with Tabs or Newlines

---
+++ 1. Python/Variables & Data Types/Strings/space.py
@@ -0,0 +1,11 @@
+print("Python")
+print("\n")
+
+print("\tPython")
+print("\n")
+
+print("Languages:\nPython\nC\nJavascript")
+print("\n")
+
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+print("\n")
