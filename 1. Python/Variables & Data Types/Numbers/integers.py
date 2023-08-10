1dcdc0d7765869963dd3ecfd4d03630f163fc59c
kinsDev <kinsleygitonga@gmail.com>
2023-08-10 20:56:23 +0300

    Numbers

---
+++ 1. Python/Variables & Data Types/Numbers/integers.py
@@ -0,0 +1,16 @@
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3/2)
+
+# square
+print(3 ** 2)
+
+#cubic
+print(3 ** 3)
+
+# Python follows BODMAS
+print(2 + 3*4)
+
+# or follows your order of specialization when you add braces
+print((2 + 3) * 4)
