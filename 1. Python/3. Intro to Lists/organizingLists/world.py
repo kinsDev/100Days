e07f112771d93bdb15e58a2dfe7469df4d809ad6
kinsDev <kinsleygitonga@gmail.com>
2023-08-20 01:28:11 +0300

    lists

---
+++ 1. Python/3. Intro to Lists/organizingLists/world.py
@@ -0,0 +1,28 @@
+# Places I would like to visit
+places = ['nairobi', 'newyork', 'moscow', 'paris', 'qatar']
+print(places)
+print('\n')
+
+# sort the list of places in alphabetical order
+print(sorted(places))
+print('\n')
+
+# let me show you that the list is still in original form
+print(places)
+print('\n')
+
+# Use sorted() to print your list in reverse alphabetical order without changing the order of the original list
+print(sorted(places, reverse = True))
+print(places)
+print('\n')
+
+# using reverse to change the order of my list and printing the new order of the list
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+print('\n')
+
+# sort the list in alphabetical order
+places.sort()
+print(places)
