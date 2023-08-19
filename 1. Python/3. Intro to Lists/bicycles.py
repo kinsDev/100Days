3177f65c4edc2ba0b7e0a7b8a65ac134ecf9b17a
kinsDev <kinsleygitonga@gmail.com>
2023-08-19 11:35:14 +0300

    a list that contains a few kinds of bicycles

---
+++ 1. Python/3. Intro to Lists/bicycles.py
@@ -0,0 +1,9 @@
+# a list that contains a few kinds of bicycles
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#accessing elemennt in a list
+print(bicycles[0])
+
+# modifying an element in a list
+print(bicycles[0].title())
