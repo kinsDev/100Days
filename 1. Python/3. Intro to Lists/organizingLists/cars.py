c346b6ca8b292155145ff55d14fb4de1ede01dfe
kinsDev <kinsleygitonga@gmail.com>
2023-08-20 00:44:19 +0300

    organizing lists

--- 1. Python/3. Intro to Lists/organizingLists/cars.py
+++ 1. Python/3. Intro to Lists/organizingLists/cars.py
@@ -6,3 +6,13 @@ print(cars)
 # lets sort the list of cars n reverse alphabetical order
 cars.sort(reverse = True)
 print(cars)
+print('\n')
+
+# Printing a List in Reverse Order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.reverse()
+print(cars)
+
+# finding the length of a list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
