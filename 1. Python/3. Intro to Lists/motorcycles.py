e636ac2649fdded7de684b6994117f409dda3374
kinsDev <kinsleygitonga@gmail.com>
2023-08-19 12:28:27 +0300

    surfing through lists

--- 1. Python/3. Intro to Lists/motorcycles.py
+++ 1. Python/3. Intro to Lists/motorcycles.py
@@ -28,4 +28,24 @@ motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
 # Removing Elements from a List
-#Removing an Item Using the del Statement
+# Removing an Item Using the del Statement
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
+
+# Removing an item using the pop() method and then re-using it on your code
+# pop method removes the last item in your list
+motorcycles = ['Yamaha', 'Speed', 'honda', 'suzuki']
+removed_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(removed_motorcycle)
+# we can pop at any position of list by including the position we wanna pop at
+pop2 = motorcycles.pop(2)
+print(pop2)
+
+# We can also remove an item by value
+motorcycles = ['Yamaha', 'Speed', 'honda', 'suzuki']
+removed = motorcycles.remove('Speed')
+print("I no longer want the " + str(removed) + " motorcyle")
+print(removed)
+print(motorcycles)
