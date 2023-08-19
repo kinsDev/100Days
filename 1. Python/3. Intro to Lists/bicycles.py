bab20a8a1c32affb94aef9adf5b92d691bb123a9
kinsDev <kinsleygitonga@gmail.com>
2023-08-19 11:39:39 +0300

    Using Individual Values from a List

--- 1. Python/3. Intro to Lists/bicycles.py
+++ 1. Python/3. Intro to Lists/bicycles.py
@@ -7,3 +7,7 @@ print(bicycles[0])
 
 # modifying an element in a list
 print(bicycles[0].title())
+
+# Using Individual Values from a List
+message = "I love the " + bicycles[1] + " bicycle model. " + "And I think it is as fast as a " + bicycles[0].title() + "."
+print(message)
