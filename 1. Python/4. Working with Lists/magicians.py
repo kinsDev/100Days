a4304ab4e95faac78d3287e9c0aa914eaaa6ae2f
kinsDev <kinsleygitonga@gmail.com>
2023-08-21 13:48:12 +0300

    I'm really enjoying working with loops on lists

--- 1. Python/4. Working with Lists/magicians.py
+++ 1. Python/4. Working with Lists/magicians.py
@@ -2,5 +2,14 @@
 magicians = ['kinsley', 'kaimenyi', 'gitonga']
 for magician in magicians:
     print(magician.title())
+print('\n')
 # we are picking elements in the magicians lists and storing it in the magician variable
 # and then we are printing out the magician variable
+
+
+# Doing more work within a for loop
+# lets add a message to each magician telling them that they did a good job
+magicians = ['john', 'doe', 'monty', 'pep8']
+for magician in magicians:
+    print(magician.title() + ", wonderful performance today!")
+    print("I can't wait to see you next time, " + magician + "\n")# lets add a new second line of message to our magicians
