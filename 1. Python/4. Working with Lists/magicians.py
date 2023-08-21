36d6587a49dfba520820e280ed88bcf584c83a4f
kinsDev <kinsleygitonga@gmail.com>
2023-08-21 13:01:14 +0300

    Looping Through an Entire List

---
+++ 1. Python/4. Working with Lists/magicians.py
@@ -0,0 +1,6 @@
+# Looping through an entire list
+magicians = ['kinsley', 'kaimenyi', 'gitonga']
+for magician in magicians:
+    print(magician.title())
+# we are picking elements in the magicians lists and storing it in the magician variable
+# and then we are printing out the magician variable
