2eb649f407d1518bde48b4109618afd2f5005452
kinsDev <kinsleygitonga@gmail.com>
2023-08-21 14:16:35 +0300

    for the love of pizza

---
+++ 1. Python/4. Working with Lists/pizza.py
@@ -0,0 +1,5 @@
+# print every pizza in a list of pizzas using a for loop
+pizzas = ['beef', 'chicken', 'hawaian']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza")
+print("\nI really love pizza!")
