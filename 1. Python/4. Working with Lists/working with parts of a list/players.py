ddb9fcf1f731abfcde547eb7f6570170934b5e64
kinsDev <kinsleygitonga@gmail.com>
2023-08-28 15:19:03 +0300

    picking elements in different parts of a list

---
+++ 1. Python/4. Working with Lists/working with parts of a list/players.py
@@ -0,0 +1,19 @@
+# Slicing a List
+players = ["Messi", "Haaland", "Neymar", "Mbappe", "Ronaldo", "Lewandoski"]
+# from the beginning to the 3rd person on the list
+print(players[0:3])
+
+# from the first person to the fourth person on the list
+print(players[1:4])
+
+# from beginning to the fourth person on the list
+print(players[:4])
+
+# after the fourth person in the list to the end
+print(players[4:])
+
+# slicing the last three players in the roaster
+print(players[-3:])
+
+# slicing the first three players in the roaster
+print(players[:-3])
