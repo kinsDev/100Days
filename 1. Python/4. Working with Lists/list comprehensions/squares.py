ea1448895f5b46faac5c84614cda55ed53b6c466
kinsDev <kinsleygitonga@gmail.com>
2023-08-28 14:40:09 +0300

    list comprehension

---
+++ 1. Python/4. Working with Lists/list comprehensions/squares.py
@@ -0,0 +1,5 @@
+# A list comprehensions combines the for loop with the value to be put in the list.
+# This is the procedure
+## variable to store the squares, followed by the value in its squared format, for loop, range of values and that's it
+squares = [value **2 for value in range(1, 11)]
+print(squares)
