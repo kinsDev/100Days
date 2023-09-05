0ba96f1f57ecb273c870c9d00389e9b65a713ad0
kinsDev <kinsleygitonga@gmail.com>
2023-09-05 06:28:34 +0300

    listing players

---
+++ 1. Python/4. Working with Lists/working with parts of a list/exercise.py
@@ -0,0 +1,10 @@
+# Slicing a List
+players = ["Messi", "Haaland", "Neymar", "Mbappe", "Ronaldo", "Lewandoski", "capitan", "seb", "cancelo"]
+# the first three players in the list are
+print(players[:3])
+
+# three players from the middle of the list are
+print(players[3:6]) # to accomplish that, I will have to specify the index I want to include and the index that I want to start excluding
+
+# the last three players in the list
+print(players[-3:])
