b2f20679cb82749e95e673afab1758ae5eebd75b
kinsDev <kinsleygitonga@gmail.com>
2023-09-05 07:03:16 +0300

    modifying tuples

--- 1. Python/4. Working with Lists/Tuples/dimensions.py
+++ 1. Python/4. Working with Lists/Tuples/dimensions.py
@@ -1,4 +1,20 @@
 # dimensions of a rectangle that do not really change
-dimension = (200, 50)
-print(dimension[0]) 
-print(dimension[1])
+dimensions = (200, 50)
+print(dimensions[0]) 
+print(dimensions[1])
+print('\n')
+
+
+# Looping through all values in a tuple
+print("Original dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+print('\n')
+
+
+# Writing over a tuple
+# Although you can’t modify a tuple, you can assign a new value to the variable that holds a tuple
+dimensions = (400, 100)
+print("Modified dimensions: ")
+for dimension in dimensions:
+    print(dimension)
