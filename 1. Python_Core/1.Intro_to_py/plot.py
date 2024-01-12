02efb0d5e50d1423354679446aaae395cec41293
kinsDev <kinsleygitonga@gmail.com>
2024-01-12 09:12:05 +0300

    Python Core - Jetbrains academy

---
+++ 1. Python_Core/1.Intro_to_py/plot.py
@@ -0,0 +1,8 @@
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+updated_names = names.replace(" ", "_")
+usernames = []
+
+# write your for loop here
+for name in updated_names:
+    usernames.append(name)
+print(usernames)
